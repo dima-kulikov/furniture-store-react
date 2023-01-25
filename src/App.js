782a4c18f5f9db286051723d1d0ce03f0b61a5f0
Dima <skaizy44@gmail.com>
2023-01-25 22:09:54 +0200

    lesson 3
    lesson 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,93 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
+class App extends React.Component{
+constructor(props) {
+  super(props)
+  this.state = {
+    items:[
+      {
+        id:1,
+        title: 'Стул серый',
+        img: './img/chairs-1.jpg',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'chairs',
+        price: '34.99'
+      },
+      {
+        id:2,
+        title: 'Стол',
+        img: './img/tables-1.jpg',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'tables',
+        price: '134.99'
+      },
+      {
+        id:3,
+        title: 'Диван',
+        img: '',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'sofa',
+        price: '234.99'
+      },
+      {
+        id:4,
+        title: 'Лампа',
+        img: '',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'light',
+        price: '44.20'
+      },
+      {
+        id:5,
+        title: 'Стул черный',
+        img: './img/chairs-1.jpg',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'chairs',
+        price: '56.49'
+      },
+      {
+        id:6,
+        title: 'Стол',
+        img: './img/tables-2.jpg',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'tables',
+        price: '199.99'
+      },
+      {
+        id:7,
+        title: 'Диван',
+        img: '',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'sofa',
+        price: '456.25'
+      },
+      {
+        id:8,
+        title: 'Лампа',
+        img: '',
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, illum at?",
+        category: 'light',
+        price: '72.15'
+      },
+    ]
+  }
+}
+
+
+  render() {
   return (
-    <div>
-     44
+    <div className="wrapper">
+     <Header/>
+     <Footer/>
     </div>
   );
 }
+}
 
 export default App;
+
+
+// https://www.youtube.com/watch?v=5XAz9-diwuA&list=PL0lO_mIqDDFVfIjOW2NsBaDYXB_ZwDB0p&index=3
+// 3-35
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        Тестовий сайт на React &copy;
+        
+    </footer>
+  )
+}
