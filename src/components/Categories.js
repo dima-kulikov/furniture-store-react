e051ab98ce448cd8e9ec82acb043d3cf3c7994cf
Dima <skaizy44@gmail.com>
2023-01-30 19:46:33 +0200

    н
    рнр

---
+++ src/components/Categories.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+export class Categories extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            categories: [
+                {
+                    key:'all',
+                    name: 'Все'
+                },
+                {
+                    key:'chairs',
+                    name: 'стулья'
+                },
+                {
+                    key:'tables',
+                    name: 'столы'
+                },
+                {
+                    key:'sofa',
+                    name: 'Диваны'
+                },
+                {
+                    key:'light',
+                    name: 'Лампы'
+                }
+            ]
+        }
+    }
+
+  render() {
+    return (
+      <div className='categories'>
+        {this.state.categories.map(el => (
+            <div key={el.key}>{el.name}</div>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Categories
