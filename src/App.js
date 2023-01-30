b86525f433850545d595bd1e6ddc5312c1435d26
Dima <skaizy44@gmail.com>
2023-01-30 20:10:04 +0200

    3232
    3232323

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ constructor(props) {
   super(props)
   this.state = {
     orders:[],
+    currentItems:[],
     items:[
       {
         id:1,
@@ -76,9 +77,26 @@ constructor(props) {
       },
     ]
   }
+  this.state.currentItems = this.state.items
   this.addToOrder = this.addToOrder.bind(this)
   this.deleteOrder = this.deleteOrder.bind(this)
+  this.chooseCategory = this.chooseCategory.bind(this)
 }
+
+
+chooseCategory(category){
+  if(category === 'all'){
+    this.setState({
+      currentItems: this.state.items
+    })
+    return
+  }
+
+  this.setState({
+    currentItems: this.state.items.filter(el => el.category === category)
+  })
+}
+
 
 deleteOrder(id) {
   this.setState({orders: this.state.orders.filter( el => el.id !== id)})
@@ -103,8 +121,8 @@ addToOrder(item) {
   return (
     <div className="wrapper">
      <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
-     <Categories />
-     <Items items={this.state.items} onAdd={this.addToOrder}/>
+     <Categories chooseCategory={this.chooseCategory}/>
+     <Items items={this.state.currentItems} onAdd={this.addToOrder}/>
      <Footer/>
     </div>
   );
